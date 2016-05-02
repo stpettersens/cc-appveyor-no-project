cfe59c2f643852e84cea9d6bd9b662194209bd99
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-05-03 00:57:29 +0100

    ...

--- path.cc
+++ path.cc
@@ -1,6 +1,3 @@
-#ifdef __win32
-#include "stdafx.h"
-#endif
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -11,7 +8,7 @@ int main() {
   std::cout << my_path << std::endl;
   std::replace(my_path.begin(), my_path.end(), '/', '\\');
   std::cout << my_path << std::endl;
-  // my_path = std::regex_replace(my_path, std::regex("\\"), "/");
-  // std::cout << my_path << std::endl;
+  my_path = std::regex_replace(my_path, std::regex("\\"), "/");
+  std::cout << my_path << std::endl;
   return 0;
 }
