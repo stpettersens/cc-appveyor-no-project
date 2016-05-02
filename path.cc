9f89bba481b932c2773c9f13f5782e78c9f4b4be
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-05-03 00:41:36 +0100

    ...

--- path.cc
+++ path.cc
@@ -4,11 +4,14 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <regex>
 
 int main() {
   std::string my_path = "foo/bar/oh/yeah";
   std::cout << my_path << std::endl;
   std::replace(my_path.begin(), my_path.end(), '/', '\\');
   std::cout << my_path << std::endl;
+  // my_path = std::regex_replace(my_path, std::regex("\\"), "/");
+  // std::cout << my_path << std::endl;
   return 0;
 }
